09f767a893682a8e15acac37fb376b8abe6856ea
huduny <87359710+huduny@users.noreply.github.com>
2021-07-25 21:16:08 +0900

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,7 @@
+if True:
+    print("code1")
+    print("code2")
+print("code3")
+if True:
+  print("code1")
+  print("code2")
---
+++ conditional.rb
@@ -0,0 +1,8 @@
+if true
+  puts("code1")
+  puts("code2")
+end
+  puts("code3")
+if True
+  puts("code1")
+  puts("code2")
