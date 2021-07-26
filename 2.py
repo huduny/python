d886a39da8c4aac1c53f3985dabe67564d6aea39
huduny <87359710+huduny@users.noreply.github.com>
2021-07-26 17:23:06 +0900

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,6 @@
+input = 11
+real = 22
+if real == input:
+    print("Hello!")
+else:
+    print("Who are you?")
---
+++ try_by_myself.py
@@ -0,0 +1,8 @@
+id="chl844"
+pw= "11"
+a=input("아이디를 입력해주세요: ")
+b=input("비밀번호를 입력해주세요:")
+if id == a and pw==b:
+print("정삭적으로 로그인 되었습니다.")
+elif a != id or pw != b:
+print("아이디와 비밀번호를 다시 확인해주세요")
