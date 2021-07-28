dd3df0bece00ed34e63f82f895cb3961b8cca573
huduny <87359710+huduny@users.noreply.github.com>
2021-07-28 13:57:43 +0900

    Add files via upload

---
+++ co1.py
@@ -0,0 +1,17 @@
+#print(1)
+print(2)
+
+'''
+# conditional example
+# egoing
+'''
+
+input = 33 # user input password
+real_egoing = 11
+#real_k8805 = "ab"
+if real_egoing == input:
+    print("Hello!, egoing")
+#elif real_k8805 == input:
+#    print("Hello!, k8805?")
+else:
+    print("who are you?")
