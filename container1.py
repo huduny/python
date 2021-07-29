e023bd619edd34885ef8d23fa93bd89f4a5e0c9a
huduny <87359710+huduny@users.noreply.github.com>
2021-07-29 23:31:31 +0900

    Add files via upload

---
+++ container1.py
@@ -0,0 +1,11 @@
+print(type('egoing'))
+name = 'egoing'
+print(name)
+print(type(['egoing', 'leezche', 'graphittie']))
+names = ['egoing', 'leezche', 'graphittie']
+print(names)
+print(names[1])
+egoing = ['programmer', 'seoul', 25, False]
+print(egoing)
+egoing[1] = 'busan'
+print(egoing)
