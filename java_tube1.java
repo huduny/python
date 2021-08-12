8d8b2bc63c22eb3bc82065c6e97a4abf64ab4503
huduny <87359710+huduny@users.noreply.github.com>
2021-08-12 20:38:55 +0900

    Add files via upload

---
+++ java_tube1.java
@@ -0,0 +1,26 @@
+package JAVA_YOUTUBE;
+
+public class java_tube1 {
+
+	public static void main(String[] args) {
+		/*
+		 * 자바는 정수값, 실수값, 문자값, 진리값
+		 * 정수값
+		 * 
+		 * 
+		 * 
+		 */
+		
+		System.out.write('3');
+		System.out.flush();
+
+		
+		 
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ java_tube2.java
@@ -0,0 +1,29 @@
+package JAVA_YOUTUBE;
+
+public class java_tube2 {
+
+	public static void main(String[] args) {
+		int kor1, kor2, kor3;
+		int total;
+		float avg;
+		
+		kor1 =50;
+		kor2 =60;
+		kor3 =80;
+		
+		total = kor1 + kor2 + kor3;
+		avg = total/3;
+		
+		//-------------출력부분-----------------------------
+		System.out.println("┌───────────────────────────────┐");
+		System.out.print("└──────────\\성적출력\\─────────────┘\n");
+		
+		System.out.print("\t      국어1 : 0 \n");
+		System.out.print("\t     국어2 : 0 \n");
+		System.out.print("\t      국어3 : 0 \n");
+		System.out.print("\t      총점 : 0  \n");
+		System.out.print("\t  평균 : 0  \n");
+		
+	}
+
+}
