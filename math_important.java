f9279b5f971876e73d3fb5f4c2058ecb80e2b191
huduny <87359710+huduny@users.noreply.github.com>
2021-08-18 20:45:01 +0900

    Add files via upload

---
+++ math_important.java
@@ -0,0 +1,21 @@
+package JAVA_YOUTUBE;
+
+import java.util.Scanner;
+
+public class math_important {
+
+	public static void main(String[] args) {
+		int total = 0;
+		
+//		for(int i=0; i<5; i++)
+//			total += 3+i;
+		
+//		n(a+l)/2
+		total = 5*(3+7)/2;
+		
+		System.out.printf("total is %d\n", total);
+		
+
+ 
+ }
+}
